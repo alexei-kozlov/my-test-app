76a2f443fcf35c87543f0348354c1c40eac30878
Alexey Kozlov <63234749+alexei-kozlov@users.noreply.github.com>
2022-12-14 10:49:20 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,16 @@ function initMap() {
   document.getElementById('submit').addEventListener('click', () => {
     calculateAndDisplayRoute(directionsService, directionsRenderer);
   });
+  document.getElementById('start').addEventListener('keyup', function (e) {
+		if (e.keyCode === 13) {
+			calculateAndDisplayRoute(directionsService, directionsRenderer);
+		}
+  });
+  document.getElementById('end').addEventListener('keyup', function (e) {
+		if (e.keyCode === 13) {
+			calculateAndDisplayRoute(directionsService, directionsRenderer);
+		}
+  });
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
@@ -23,15 +33,15 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
           directionsRenderer.setDirections(response);
           const route = response.routes[0];
           const summaryPanel = document.getElementById('directions-panel');
+		  summaryPanel.classList.add('active');
           summaryPanel.innerHTML = '';
-          // summaryPanel.style.textAlign = 'center';
 
           for (let i = 0; i < route.legs.length; i++) {
             const routeSegment = i + 1;
             summaryPanel.innerHTML +=
-                '<b>Route Segment: ' + routeSegment + '</b><br><ins>FROM:</ins> ';
-            summaryPanel.innerHTML += route.legs[i].start_address + '<br><ins>TO:</ins> ';
-            summaryPanel.innerHTML += route.legs[i].end_address + '<br><ins>DISTANCE:</ins> ';
+                '<b>Route Segment: ' + routeSegment + '</b><br><ins>From:</ins> ';
+            summaryPanel.innerHTML += route.legs[i].start_address + '<br><ins>To:</ins> ';
+            summaryPanel.innerHTML += route.legs[i].end_address + '<br><br>';
             summaryPanel.innerHTML += route.legs[i].distance.text;
           }
         } else {
